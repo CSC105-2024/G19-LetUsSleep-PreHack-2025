960b2e908d293c90d233df442ba8caf15167c057
BBelleysp <yossapan.r@gmail.com>
2025-05-17 18:56:01 +0700

    feat: add React Router and configure routes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,102 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx';
+import SearchJob from './pages/SearchJob.jsx';
+import JobDetails from './pages/JobDetails.jsx';
+import FilterBar from './components/FilterBar.jsx';
+import EachCompanyJobs from './pages/EachCompanyJobs.jsx';
+import RecommendCompany from './components/RecommendCompany.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import UserDetail from './pages/UserDetail.jsx';
+import CompanyDashboard from './pages/CompanyDashboard.jsx';
+import CompanyProfileForm from './pages/CompanyProfileForm.jsx';
+import CompanyJobPostForm from './pages/CompanyJobPostForm.jsx';
+import ConfirmPopUp from './components/ConfirmPopUp.jsx';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App />, 
+    children: [
+            {
+                index: true,
+                element: <Home/>,
+            },
+            {
+                path: "/SearchJob",
+                element: <SearchJob/>,
+            },
+            
+            {
+                path: "/JobDetails",
+                element: <JobDetails/>,
+            },
+            {
+                path: "/FilterBar",
+                element: <FilterBar/>,
+            },
+            {
+                path: "/EachCompanyJobs",
+                element: <EachCompanyJobs/>,
+            },
+            {
+                path: "/RecommendCompany",
+                element: <RecommendCompany/>,
+            },
+            {
+                path: "/Login",
+                element: <Login/>,
+            },
+            {
+                path: "/Register",
+                element: <Register/>,
+            },
+            {
+                path: "/UserDetail",
+                element: <UserDetail/>,
+            },
+            {
+                path: "/CompanyDashboard",
+                element: <CompanyDashboard/>,
+            },
+            {
+                path: "/CompanyProfileForm",
+                element: <CompanyProfileForm/>,
+            },
+            {
+                path: "/CompanyJobPostForm",
+                element: <CompanyJobPostForm/>,
+            },
+            {
+                path: "/ConfirmPopUp",
+                element: <ConfirmPopUp/>,
+            },
+            {
+                path: "/Navbar",
+                element: <Navbar/>,
+            },
+            {
+                path: "/Footer",
+                element: <Footer/>,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFoundPage/>,
+    },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //<StrictMode>
+    <RouterProvider router={router} /> 
+  //</StrictMode>,
 )
